0d92d29cf3a55d7e8e1fdef740efebafb23dfc61
MattBlueRich <S240530@UOS.AC.UK>
2024-07-12 18:22:01 +0100

    Add Item Drop Sprites

--- Assets/Scripts/ItemDrop.cs
+++ Assets/Scripts/ItemDrop.cs
@@ -14,10 +14,14 @@ public class ItemDrop : MonoBehaviour
 {
     public dropType itemDrop;
     public int scoreValue;
+    public Sprite[] itemSprites; 
+    private SpriteRenderer spriteRenderer;
 
     // The Start() function handles the random drop selection.
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         int randomNo = Random.Range(0, 20); // Pick a random value.
 
         // [ Extra Life Drop ]
@@ -25,6 +29,8 @@ public class ItemDrop : MonoBehaviour
         if (randomNo < 8 /* && Player has less than maxLives */)
         {
             itemDrop = dropType.extraLife;
+            spriteRenderer.sprite = itemSprites[0];
+
         }
         // [ Fruit Random Drop ]
         // 61%
@@ -35,18 +41,21 @@ public class ItemDrop : MonoBehaviour
             {
                 itemDrop = dropType.cherry100;
                 scoreValue = 100;
+                spriteRenderer.sprite = itemSprites[1];
             }
             // 19% - 300
             else if(randomNo >= 15 && randomNo < 19) 
             {
                 itemDrop = dropType.strawberry300;
                 scoreValue = 300;
+                spriteRenderer.sprite = itemSprites[2];
             }
             // 9% - 500
             else if(randomNo >= 19)
             {
                 itemDrop = dropType.orange500;
                 scoreValue = 500;
+                spriteRenderer.sprite = itemSprites[3];
             }
         }       
     }
